0c3c6540a43a05ad8f510ddc0bdd1c8a8bf1b78d
Cynthia <ex09190919@yahoo.com.tw>
2018-06-26 21:37:39 +0800

    add restaurant index action and view for public user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 透過路由設定，產生對外開放的網址入口
+  # 前面加 /，就是將首頁指向 RestaurantsController 的 index action。
+  root "restaurants#index"
+
 end
