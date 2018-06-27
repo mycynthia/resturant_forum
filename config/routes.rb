11282f0bb9addbd2156cfe92ddf8362265fe7eac
Cynthia <ex09190919@yahoo.com.tw>
2018-06-27 14:25:16 +0800

    add restaurant index and view for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # 透過路由設定，產生對外開放的網址入口
+  # 前台入口一般使用者，透過路由設定，產生對外開放的網址入口
   # 前面加 /，就是將首頁指向 RestaurantsController 的 index action。
   root "restaurants#index"
+
+  # 後台入口管理者是採用namespace來區隔建立admin名稱可以任意更改
+  namespace :admin do
+    root "restaurants#index"
+  end
 
 end
