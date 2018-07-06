7c63a243ae096384906c94bfbfa308553abb1130
Cynthia <ex09190919@yahoo.com.tw>
2018-07-06 18:15:20 +0800

    user can upload avatar and also modify user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :intro)
+    params.require(:user).permit(:name, :intro, :avatar)
   end
 
 end
