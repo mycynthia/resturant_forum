3e8f418cb68e925f02518193bd8fdb21fbd1a9c4
Cynthia <ex09190919@yahoo.com.tw>
2018-07-10 12:54:38 +0800

    display commenter's name on restaurant show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # 客製化devise gem 加username
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   private
     def authenticate_admin
