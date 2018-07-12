d8671fef8bdf999930e5cad087da0954ad85134a
Cynthia <ex09190919@yahoo.com.tw>
2018-07-12 22:18:39 +0800

    modify restaurant relation setting

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,6 @@ class Restaurant < ApplicationRecord
   # 當Restaruant 物件被刪除時，順便出除依賴的Comment
   has_many :comments, dependent: :destroy
   # 餐廳被許多使用者收藏
-  has_many :favorites, dependent :destroy
+  has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
 end
