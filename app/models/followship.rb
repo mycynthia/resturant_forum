78648189a07867f3f6f7571c01617bc28e0d8f09
Cynthia <ex09190919@yahoo.com.tw>
2018-07-17 09:00:03 +0800

    validate uniqueness of following_id in user_id scope

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,8 @@
 class Followship < ApplicationRecord
   belongs_to :user #可省略, class_name: "User", primary_key: "id", foreign_key: "user_id"
   belongs_to :following, class_name: "User" #可省略, primary_key: "id", foregin_key: "following_id"
+
+  # 每個 User 只能追蹤另一個 User 一次
+  validates :following_id, uniqueness: { scope: :user_id }
+
 end
