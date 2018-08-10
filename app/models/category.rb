154ffe6dfb3ec8ff82f4380532a588063af78ef7
Cynthia <ex09190919@yahoo.com.tw>
2018-08-10 15:44:01 +0800

    test error and exception different

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   validates_presence_of :name
   # 如果分類下已有餐廳，就不允許刪除分類(刪除時會拋出error)
   has_many :restaurants, dependent: :restrict_with_error
+  # 有關聯物件的話，向擁有者拋出異常
+  # has_many :restaurants, dependent: :restrict_with_exception
 end
